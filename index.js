a6cb83ab5b7eccf1e2b769380db0a20378b96848
zhangxp <zhangxp@dazhuanjia.com>
2021-01-12 20:05:00 +0800

    fix: modifiy directoryName

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 import 'react-native-gesture-handler'
  import {AppRegistry} from 'react-native';
 // import App from './App';
-import AuthorizationPage from './src/01/AuthorizationPage'
+import AuthorizationPage from './src/practices/AuthorizationPage'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import NavigationTestPage from './src/02/NavigationTestPage'
+import NavigationTestPage from './src/practices/NavigationTestPage'
 import {name as appName} from './app.json'
 
 export default function App() {
