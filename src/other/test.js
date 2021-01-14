46169119de7cdc6ac6298ea90e0107f1790600de
mizxinp <mizxinp@gmail.com>
2021-01-14 20:47:26 +0800

    test: git

---
+++ src/other/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function test() {
+    return (
+        <View>
+            <Text>test</Text>
+        </View>
+    )
+}
+
+export default test
